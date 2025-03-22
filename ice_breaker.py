6969a654e87f8b91527f2502aa6667ff359c9465
sjainlucky <shudhatma1997@gmail.com>
2025-03-22 20:20:23 +0530

    debug

--- ice_breaker.py
+++ ice_breaker.py
@@ -9,11 +9,6 @@ from third_parties.linkedin import scrape_linkedin_profile
 
 load_dotenv()
 
-information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman known for his key roles in Tesla, SpaceX, and Twitter (which he rebranded as X). Since 2025, he has been a senior advisor to United States president Donald Trump and the de facto head of the Department of Government Efficiency (DOGE). Musk is the wealthiest person in the world; as of March 2025, Forbes estimates his net worth to be $320 billion USD.
-
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada, whose citizenship he had inherited through his mother. He graduated from the University of Pennsylvania in the U.S. before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002 for $1.5 billion. That year, Musk also became a U.S. citizen.
-"""
-
 if __name__ == "__main__":
 
     summary_template = """
